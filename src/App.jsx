a565b7d5bae83068706f43e92ac4102a84878b95
cauagjesus <dejesuscaua20@gmail.com>
2023-11-24 20:14:25 -0300

    finalizei

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './page/Home/Home';
 import Login from './page/Login/login';
 
-
 function App() {
-  const [loggedInUser, setLoggedInUser] = useState(null);
+  
+  const initialUser = sessionStorage.getItem('loggedInUser')
+    ? JSON.parse(sessionStorage.getItem('loggedInUser'))
+    : null;
+
+  const [loggedInUser, setLoggedInUser] = useState(initialUser);
+
+  useEffect(() => {
+    sessionStorage.setItem('loggedInUser', JSON.stringify(loggedInUser));
+  }, [loggedInUser]);
 
   return (
-    
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login setLoggedInUser={setLoggedInUser} />} />
